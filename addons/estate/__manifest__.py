4a5a617d782f501ba7633bd08e551e22f5bb22a6
alexMartJu <alex.mart.juan@gmail.com>
2025-12-14 14:50:56 +0100

    Add property type/tag/offer models, views and access rights (Chapter 7)

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -29,6 +29,9 @@ This is a minimal example module intended for learning and local testing with Od
   'data': [
     'security/ir.model.access.csv',
     'views/estate_property_views.xml',
+    'views/estate_property_type_views.xml',
+    'views/estate_property_tag_views.xml',
+    'views/estate_property_offer_views.xml',
     'views/estate_menus.xml',
   ],
 }
