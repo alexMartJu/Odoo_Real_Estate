8eb2f26ff3850f6d766db9b39f8656e844b17d49
alexMartJu <alex.mart.juan@gmail.com>
2025-12-18 12:53:15 +0100

    Add kanban view for properties (Chapter 14)

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -32,6 +32,7 @@ This is a minimal example module intended for learning and local testing with Od
     'views/estate_property_type_views.xml',
     'views/estate_property_tag_views.xml',
     'views/estate_property_views.xml',
+    'views/estate_property_kanban_views.xml',
     'views/estate_menus.xml',
     'views/res_users_views.xml',
   ],
